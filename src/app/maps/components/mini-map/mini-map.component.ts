a34753f62790b9c30fff9ac2871702cdfff0c29c
Jhonny <jhonnycgarcia@gmail.com>
2024-10-17 02:17:33 -0500

    Crear los componentes y rutas

---
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'maps-mini-map',
+  templateUrl: './mini-map.component.html',
+  styles: ``
+})
+export class MiniMapComponent {
+
+}
