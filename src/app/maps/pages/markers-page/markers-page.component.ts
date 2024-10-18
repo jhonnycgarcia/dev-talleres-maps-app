ff328693d4081f16a6535cc5c39898e02ce30f32
Jhonny <jhonnycgarcia@gmail.com>
2024-10-17 22:09:25 -0500

    Marcadores en el mapa

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
+import { environment } from '../../../../environments/environments';
 
 @Component({
   selector: 'maps-markers-page',
   templateUrl: './markers-page.component.html',
-  styles: ``
+  styleUrl: './markers-page.component.css'
 })
-export class MarkersPageComponent {
+export class MarkersPageComponent implements AfterViewInit, OnDestroy {
+
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  public map?: Map;
+  public lnglat: LngLat = new LngLat(-66.90260060005852, 10.483781022919501);
+
+  ngAfterViewInit(): void {
+    if(!this.divMap) { throw new Error('divMap is not defined'); }
+    this.map = new Map({
+      accessToken: environment.mapboxKey,
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lnglat, // starting position [lng, lat]
+      zoom: 13, // starting zoom
+    });
+
+    // const marketHtml = document.createElement('div');
+    // marketHtml.innerHTML = 'Jhonny García';
+
+    // const marker = new Marker({
+    //     color: 'red',
+    //     // element: marketHtml,
+    //   })
+    //   .setLngLat(this.lnglat)
+    //   .addTo(this.map);
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
+
 
 }
