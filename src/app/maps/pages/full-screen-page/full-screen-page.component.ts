22d76cc7225708d1e04018bdf73d5f04570ecf6c
Jhonny <jhonnycgarcia@gmail.com>
2024-10-17 21:34:51 -0500

    ViewChild - ElementRef

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,7 +1,6 @@
-import { AfterViewInit, Component } from '@angular/core';
-import mapboxgl from 'mapbox-gl';
-
-
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map} from 'mapbox-gl';
+import { environment } from '../../../../environments/environments';
 
 @Component({
   selector: 'maps-full-screen-page',
@@ -10,10 +9,14 @@ import mapboxgl from 'mapbox-gl';
 })
 export class FullScreenPageComponent implements AfterViewInit {
 
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
   ngAfterViewInit(): void {
-    const map = new mapboxgl.Map({
-      accessToken: 'pk.eyJ1Ijoiamhvbm55Y2dhcmNpYSIsImEiOiJjbTJlMjNjMGgwMHI2MnJuNW5zdTNsOTg4In0.li0cuv5hqogIjDUnuqFq2Q',
-      container: 'map', // container ID
+    if(!this.divMap) { throw new Error('divMap is not defined'); }
+    const map = new Map({
+      accessToken: environment.mapboxKey,
+      container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
